cc710e06931f3f0045299e48b599854bfd708f10
Sharifah Asilah <sharifahasilahs@gmail.com>
2023-04-14 14:47:26 +0800

    install and setup pinia store library

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import piniaPersist from 'pinia-plugin-persist'
 import App from './App.vue'
 import router from './router'
+import naive from 'naive-ui'
 
 import './assets/main.css'
 
-import naive from 'naive-ui'
-
+const pinia = createPinia()
+pinia.use(piniaPersist)
 const app = createApp(App)
 
 app.use(router)
+.use(pinia)
 .use(naive)
 
 app.mount('#app')
